ddd08eb82c7be96b094a049abcff6d710779b7c1
Ryo Nakatani <right.rcon.f@outlook.com>
2019-09-14 16:21:28 +0900

    Delete unnecessary import

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import MySidebar from 'src/renderer/components/sidebar/Sidebar';
 import MainContents from 'src/renderer/components/router/Router';
 

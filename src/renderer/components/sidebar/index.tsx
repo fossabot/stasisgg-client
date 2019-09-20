c7df1108540e9e3a208a25194edb74e13442d288
Ryo Nakatani <right.rcon.f@outlook.com>
2019-09-20 16:44:49 +0900

    Fix some file names

---
+++ src/renderer/components/sidebar/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Sidebar from 'react-sidebar';
+import SidebarContent from 'src/renderer/components/FixedSidebar/SidebarContent';
+
+type SidebarProps = {
+  docked: boolean;
+  open: boolean;
+  shadow: boolean;
+  transitions: boolean;
+  children: React.ReactNode;
+};
+
+const MySidebar: React.FC<SidebarProps> = ({
+  docked,
+  open,
+  shadow,
+  transitions,
+  children
+}: SidebarProps) => (
+  <Sidebar
+    sidebar={<SidebarContent />}
+    docked={docked}
+    open={open}
+    shadow={shadow}
+    transitions={transitions}
+  >
+    {children}
+  </Sidebar>
+);
+
+export default MySidebar;

c80c87cab863f4afa88e3c5bb46117d1f9121931
Ryo Nakatani <right.rcon.f@outlook.com>
2019-09-29 01:51:21 +0900

    W I P

--- src/renderer/views/Preference/index.tsx
+++ src/renderer/views/Preference/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { MainPageTheme } from 'src/renderer/components/theme';
+import { Container } from '@material-ui/core';
+
+const MarginContainer = styled.div`
+  margin: 32px 0 32px 0;
+`;
 
 const Preference = (): JSX.Element => (
-  <div>This page would be a nice preference page someday.</div>
+  <MainPageTheme>
+    <MarginContainer>
+      <Container fixed>
+        This page would be a nice preference page someday.
+      </Container>
+    </MarginContainer>
+  </MainPageTheme>
 );
 
 export default Preference;

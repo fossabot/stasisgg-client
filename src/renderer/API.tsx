dbf14e768bfea161dcc53a3442247319f8067572
Ryo Nakatani <right.rcon.f@outlook.com>
2019-10-05 03:44:34 +0900

    Add API constant

---
+++ src/renderer/API.tsx
@@ -0,0 +1,8 @@
+const baseURL =
+  'https://m52z4grtsa.execute-api.ap-northeast-1.amazonaws.com/dev';
+
+const API = {
+  getPlayerProfile: `${baseURL}/get-player-profile`
+};
+
+export default API;

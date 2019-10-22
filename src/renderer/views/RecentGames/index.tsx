ddb9d491e2975b3c6637f48125d44c0aa95b3dd0
Ryo Nakatani <right.rcon.f@outlook.com>
2019-10-23 00:22:46 +0900

    Add header

--- src/renderer/views/RecentGames/index.tsx
+++ src/renderer/views/RecentGames/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Container } from '@material-ui/core';
+import { MainPageTheme } from 'src/renderer/components/theme';
+import Header from 'src/renderer/components/Header';
 
-const Home = (): JSX.Element => <div>This is your home.</div>;
+const MarginContainer = styled.div`
+  height: calc(100vh - 4em);
+  margin: 2em 0 2em 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1em;
+  * + * {
+    margin-left: 16px;
+  }
+`;
+
+const Home = (): JSX.Element => (
+  <MainPageTheme>
+    <MarginContainer>
+      <Container fixed style={{ height: '100%' }}>
+        <HeaderContainer>
+          <Header>RECENT MATCH</Header>
+        </HeaderContainer>
+      </Container>
+    </MarginContainer>
+  </MainPageTheme>
+);
 
 export default Home;

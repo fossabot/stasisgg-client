d6184e2ba985802031bb50f53e4dcd9671f96693
Ryo Nakatani <right.rcon.f@outlook.com>
2019-10-31 00:47:35 +0900

    Add a placeholder to rounded icon

--- src/renderer/components/RoundedIcon/index.tsx
+++ src/renderer/components/RoundedIcon/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import defaultImage from 'src/assets/icons/-1.png';
 
@@ -6,10 +6,20 @@ const Icon = styled.img<{
   height?: number;
   isBordered: boolean | undefined;
 }>`
-  height: ${({ height }): number => height || 100};
+  height: ${({ height }): string => (height ? `${+height}px` : '100px')};
   border-radius: 50%;
   border: ${({ isBordered }): string =>
     isBordered ? '1px solid #2c4873' : ''};
+`;
+
+const BlankIcon = styled.div<{ height?: number; isBordered?: boolean }>`
+  height: ${({ height }): string => (height ? `${+height}px` : '100px')};
+  width: ${({ height }): string => (height ? `${+height}px` : '100px')};
+  border-radius: 50%;
+  border: ${({ isBordered }): string =>
+    isBordered ? '1px solid #2c4873' : ''};
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0.24);
 `;
 
 type ProfileProps = {
@@ -18,13 +28,26 @@ type ProfileProps = {
   isBordered?: boolean;
 };
 
-const RoundedIcon: React.FC<ProfileProps> = (props: ProfileProps) => (
-  <Icon
-    height={props.height}
-    isBordered={props.isBordered}
-    src={props.iconURL}
-    onError={(e): void => (e.currentTarget.src = defaultImage)}
-  />
-);
+const RoundedIcon: React.FC<ProfileProps> = (props: ProfileProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div>
+      <Icon
+        height={props.height}
+        isBordered={props.isBordered}
+        src={props.iconURL}
+        style={{
+          display: isLoading ? 'none' : 'block'
+        }}
+        onLoad={(): void => setIsLoading(false)}
+        onError={(e): void => (e.currentTarget.src = defaultImage)}
+      />
+      {isLoading && (
+        <BlankIcon height={props.height} isBordered={props.isBordered} />
+      )}
+    </div>
+  );
+};
 
 export default RoundedIcon;

370ecedee66afe8281f41060f6c1ab8db223262a
Ryo Nakatani <right.rcon.f@outlook.com>
2019-11-04 13:54:32 +0900

    Fetch recent games list and display them but so ugly

--- src/renderer/views/RecentGames/index.tsx
+++ src/renderer/views/RecentGames/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Container } from '@material-ui/core';
+import axios from 'axios';
 import { MainPageTheme } from 'src/renderer/components/theme';
 import Header from 'src/renderer/components/Header';
 import RoleSelecter from 'src/renderer/components/RoleSelecter';
 import GameCard from 'src/renderer/components/GameCard';
 import {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  fakerMatch,
-  faker3901480925
-} from 'src/renderer/views/RecentGames/fakerMatchMock';
+  API,
+  matchesResponse,
+  OneMatchCardResponse,
+  OneMatchCardType
+} from 'src/renderer/API';
+import { PersistentStoreContainer } from 'src/renderer/containers/PersistentStore';
+// import {
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   fakerMatch,
+//   faker3901480925
+// } from 'src/renderer/views/RecentGames/fakerMatchMock';
 
 const MarginContainer = styled.div`
   height: calc(100vh - 4em);
@@ -38,7 +46,76 @@ const GamesContainer = styled.div`
   flex-direction: column;
 `;
 
+const getOneMatchCard = async (
+  region: string,
+  summonerId: string,
+  gameId: number
+): Promise<OneMatchCardType> => {
+  return new Promise((resolve, reject): void => {
+    axios
+      .get(API.getOneMatchCard, {
+        params: {
+          region: region,
+          summonerId: summonerId,
+          gameId: gameId
+        }
+      })
+      .then(r => {
+        if (OneMatchCardResponse.is(r.data)) {
+          resolve(r.data.message);
+        } else {
+          throw new Error('invalid response');
+        }
+      })
+      .catch(e => reject(e.response));
+  });
+};
+
 const Home = (): JSX.Element => {
+  const profile = PersistentStoreContainer.useContainer();
+  const [gameIds, setGameIds] = useState([0]);
+  const [games, setGames] = useState<OneMatchCardType[]>();
+  useEffect(() => {
+    async function getGameIds(): Promise<void> {
+      const ids = await axios.get(API.getMatches, {
+        params: {
+          region: profile.store.region,
+          summonerName: profile.store.summoner_name,
+          limit: 10,
+          offset: 0
+        }
+      });
+      if (matchesResponse.is(ids.data)) {
+        const games = await Promise.all(
+          ids.data.message.matchIds.map(id =>
+            axios.get(API.getOneMatchCard, {
+              params: {
+                region: profile.store.region,
+                summonerId: profile.store.summoner_id,
+                gameId: id
+              }
+            })
+          )
+        );
+        setGames(games.map(res => res.data.message));
+      }
+    }
+    getGameIds();
+  }, [
+    profile.store.region,
+    profile.store.summoner_name,
+    profile.store.summoner_id
+  ]);
+
+  // let getCardRes: OneMatchCardType[] = [];
+  // if (matchesResponse.is(getMatchesRes)) {
+  //   getCardRes = await Promise.all(
+  //     getMatchesRes.message.matchIds.map(gameId =>
+  //       getOneMatchCard(profile.store.region, profile.store.summoner_id, gameId)
+  //     )
+  //   );
+  // }
+
   return (
     <MainPageTheme>
       <MarginContainer>
@@ -48,7 +125,10 @@ const Home = (): JSX.Element => {
             <RoleSelecter />
           </HeaderContainer>
           <GamesContainer>
-            <GameCard game={faker3901480925} />
+            {games &&
+              games.map(game => (
+                <GameCard key={game.match.gameCreationUnix} game={game} />
+              ))}
           </GamesContainer>
         </PageContainer>
       </MarginContainer>
